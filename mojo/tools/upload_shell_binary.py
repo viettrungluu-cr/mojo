4737a540d1f544c55e08e92780e0a1b6efa3ecad
James Robinson <jamesr@chromium.org>
2014-11-03 13:47:59 -0800

    Use gsutil from depot_tools, provide -h in download/upload scripts
    This uses the gsutil scripts in depot_tools to upload shell binaries so
    we can do this from a buildbot and cleans up the python a bit.
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/697203002

--- mojo/tools/upload_shell_binary.py
+++ mojo/tools/upload_shell_binary.py
@@ -3,6 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import argparse
 import os
 import subprocess
 import sys
@@ -15,17 +16,38 @@ root_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 version = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
 
 sys.path.insert(0, os.path.join(root_path, "tools"))
+# pylint: disable=F0401
+import find_depot_tools
 
 binary_path = os.path.join(root_path, "out", "Release", "mojo_shell")
 
 dest = "gs://mojo/shell/" + version + "/linux-x64.zip"
 
-with tempfile.NamedTemporaryFile() as zip_file:
-  with zipfile.ZipFile(zip_file, 'w') as z:
-    with open(binary_path) as shell_binary:
-      zipinfo = zipfile.ZipInfo("mojo_shell")
-      zipinfo.external_attr = 0777 << 16L
-      zipinfo.compress_type = zipfile.ZIP_DEFLATED
-      zipinfo.date_time = time.gmtime(os.path.getmtime(binary_path))
-      z.writestr(zipinfo, shell_binary.read())
-  subprocess.check_call(["gsutil", "cp", zip_file.name, dest])
+depot_tools_path = find_depot_tools.add_depot_tools_to_path()
+gsutil_exe = os.path.join(depot_tools_path, "third_party", "gsutil", "gsutil")
+
+def upload(dry_run):
+  with tempfile.NamedTemporaryFile() as zip_file:
+    with zipfile.ZipFile(zip_file, 'w') as z:
+      with open(binary_path) as shell_binary:
+        zipinfo = zipfile.ZipInfo("mojo_shell")
+        zipinfo.external_attr = 0777 << 16L
+        zipinfo.compress_type = zipfile.ZIP_DEFLATED
+        zipinfo.date_time = time.gmtime(os.path.getmtime(binary_path))
+        z.writestr(zipinfo, shell_binary.read())
+    if dry_run:
+      print str([gsutil_exe, "cp", zip_file.name, dest])
+    else:
+      subprocess.check_call([gsutil_exe, "cp", zip_file.name, dest])
+
+def main():
+  parser = argparse.ArgumentParser(description="Upload mojo_shell binary to "+
+      "google storage")
+  parser.add_argument("-n", "--dry_run", help="Dry run, do not actually "+
+      "upload", action="store_true")
+  args = parser.parse_args()
+  upload(args.dry_run)
+  return 0
+
+if __name__ == "__main__":
+  sys.exit(main())
