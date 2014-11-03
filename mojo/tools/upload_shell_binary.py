9ea8a5f7540e656cd0c91266bd05a159b3caa598
James Robinson <jamesr@chromium.org>
2014-11-03 14:08:50 -0800

    Run git commands from inside checkout
    This script may be run from outside the mojo checkout, chdir first so
    git rev-parse returns the right thing.
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/696423002

--- mojo/tools/upload_shell_binary.py
+++ mojo/tools/upload_shell_binary.py
@@ -13,7 +13,6 @@ import zipfile
 
 root_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                             "..", "..")
-version = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
 
 sys.path.insert(0, os.path.join(root_path, "tools"))
 # pylint: disable=F0401
@@ -21,12 +20,14 @@ import find_depot_tools
 
 binary_path = os.path.join(root_path, "out", "Release", "mojo_shell")
 
-dest = "gs://mojo/shell/" + version + "/linux-x64.zip"
-
 depot_tools_path = find_depot_tools.add_depot_tools_to_path()
 gsutil_exe = os.path.join(depot_tools_path, "third_party", "gsutil", "gsutil")
 
 def upload(dry_run):
+  version = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=root_path)
+  version = version.strip()
+  dest = "gs://mojo/shell/" + version + "/linux-x64.zip"
+
   with tempfile.NamedTemporaryFile() as zip_file:
     with zipfile.ZipFile(zip_file, 'w') as z:
       with open(binary_path) as shell_binary:
