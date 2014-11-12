6d1f41d5db60e3c295ce61394bcb2154bfee620f
Michael Wasserman <msw@chromium.org>
2014-11-12 09:44:50 -0800

    Use loops directly in apptest main files.
    Use mojo::RunLoop in application_test_main.cc
    Use base::MessageLoop in application_test_main_chromium.cc
    Avoid Environment's DefaultRunLoop abstraction.
    
    This follows some code review feedback from:
    https://codereview.chromium.org/710403002/
    
    BUG=392646
    TEST=apptests still build and run fine.
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/718123002

--- mojo/application/application_test_main_chromium.cc
+++ mojo/application/application_test_main_chromium.cc
@@ -3,11 +3,12 @@
 // found in the LICENSE file.
 
 #include "base/at_exit.h"
+#include "base/logging.h"
+#include "base/message_loop/message_loop.h"
 #include "mojo/public/c/system/main.h"
 #include "mojo/public/cpp/application/application_delegate.h"
 #include "mojo/public/cpp/application/application_impl.h"
 #include "mojo/public/cpp/application/application_test_base.h"
-#include "mojo/public/cpp/environment/logging.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 
 MojoResult MojoMain(MojoHandle shell_handle) {
@@ -15,22 +16,21 @@ MojoResult MojoMain(MojoHandle shell_handle) {
   base::AtExitManager at_exit;
 
   {
-    // This RunLoop is used for init, and then destroyed before running tests.
-    mojo::Environment::InstantiateDefaultRunLoop();
+    // This loop is used for init, and then destroyed before running tests.
+    base::MessageLoop message_loop;
 
     // Construct an ApplicationImpl just for the GTEST commandline arguments.
     // GTEST command line arguments are supported amid application arguments:
     // $ mojo_shell 'mojo:example_apptests arg1 --gtest_filter=foo arg2'
     mojo::ApplicationDelegate dummy_application_delegate;
     mojo::ApplicationImpl app(&dummy_application_delegate, shell_handle);
-    MOJO_CHECK(app.WaitForInitialize());
+    CHECK(app.WaitForInitialize());
 
     // InitGoogleTest expects (argc + 1) elements, including a terminating NULL.
     // It also removes GTEST arguments from |argv| and updates the |argc| count.
     // TODO(msw): Provide tests access to these actual command line arguments.
     const std::vector<std::string>& args = app.args();
-    MOJO_CHECK(args.size() <
-               static_cast<size_t>(std::numeric_limits<int>::max()));
+    CHECK_LT(args.size(), static_cast<size_t>(std::numeric_limits<int>::max()));
     int argc = static_cast<int>(args.size());
     std::vector<const char*> argv(argc + 1);
     for (int i = 0; i < argc; ++i)
@@ -39,14 +39,13 @@ MojoResult MojoMain(MojoHandle shell_handle) {
 
     testing::InitGoogleTest(&argc, const_cast<char**>(&(argv[0])));
     mojo::test::SetShellHandle(app.UnbindShell());
-    mojo::Environment::DestroyDefaultRunLoop();
   }
 
   int result = RUN_ALL_TESTS();
 
   shell_handle = mojo::test::PassShellHandle().release().value();
   MojoResult close_result = MojoClose(shell_handle);
-  MOJO_CHECK(close_result == MOJO_RESULT_OK);
+  CHECK_EQ(close_result, MOJO_RESULT_OK);
 
   return (result == 0) ? MOJO_RESULT_OK : MOJO_RESULT_UNKNOWN;
 }
