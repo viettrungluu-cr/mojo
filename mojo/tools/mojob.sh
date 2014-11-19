19c63464de1b35ca0ff02acb06810b8f34905e96
John Abd-El-Malek <jam@chromium.org>
2014-11-19 13:35:15 -0800

    Delete old mojob.sh script.
    The Python file was switched to 10 days ago.
    
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/735413002

--- mojo/tools/mojob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright 2013 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-python "$(dirname "$0")/mojob.py" "$@"
-echo ""
-echo ""
-echo "NOTE:  mojob.sh is deprecated, use mojob.py instead. The arguments also changed order, see mojob.py --help"
